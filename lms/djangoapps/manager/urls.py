18e360d52d74d79aca0a0ca008d1e5bf5b3ace5d
Ahmad <ahmad.a.arrahman@gmail.com>
2015-01-20 03:54:48 +0200

    another try to dump users

--- lms/djangoapps/manager/urls.py
+++ lms/djangoapps/manager/urls.py
@@ -6,11 +6,13 @@ Urls for sysadmin dashboard feature
 from django.conf.urls import patterns, url
 
 from dashboard import sysadmin
+from manager.views import dump_all_users
 
 urlpatterns = patterns(
     '',
     url(r'^$', sysadmin.Users.as_view(), name="sysadmin"),
     url(r'^courses/?$', sysadmin.Courses.as_view(), name="sysadmin_courses"),
+    url(r'^dump_all_users/?$', dump_all_users, name="dump_all_users"),
     url(r'^staffing/?$', sysadmin.Staffing.as_view(), name="sysadmin_staffing"),
     url(r'^gitlogs/?$', sysadmin.GitLogs.as_view(), name="gitlogs"),
     url(r'^gitlogs/(?P<course_id>.+)$', sysadmin.GitLogs.as_view(),
--- lms/djangoapps/manager/views.py
+++ lms/djangoapps/manager/views.py
@@ -1,4 +1,5 @@
-from django.http import Http404
+from django.http import Http404, HttpResponse
+
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST, require_GET
@@ -7,7 +8,8 @@ from util.json_request import JsonResponse
 from django.core.urlresolvers import reverse
 from edxmako.shortcuts import render_to_response
 
-
+from django.contrib.auth.models import User
+from . import unicodecsv as csv
 import shoppingcart
 
 
@@ -39,3 +41,19 @@ def index(request):
         context['form'] = form
 
     return render_to_response('manager.html', context)
+
+@login_required
+def dump_all_users(request):
+    """
+    download all users data needed to migrate them
+    """
+    users = User.objects.all()
+    response = HttpResponse(mimetype='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="users.csv"'
+    field_names = ['username', 'email', 'password', 'is_active', 'is_staff', 'is_superuser', 'name', 'level_of_education', 'gender', 'mailing_address', 'city', 'country', 'goals', 'year_of_birth']
+    writer = csv.DictWriter(response, fieldnames=field_names)
+    # writer.writeheader()
+    for user in users:
+        writer.writerow({'username': user.username, 'email': user.email, 'password': user.password, 'is_active': user.is_active, 'is_staff': user.is_staff, 'is_superuser': user.is_superuser, 'name': user.profile.name, 'level_of_education': user.profile.level_of_education, 'gender': user.profile.gender, 'mailing_address': user.profile.mailing_address, 'city': user.profile.city, 'country':user.profile.country, 'goals': user.profile.goals, 'year_of_birth': user.profile.year_of_birth})
+
+    return response
