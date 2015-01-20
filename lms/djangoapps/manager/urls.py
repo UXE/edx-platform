f79a67ed312b3dcaee86738a4b4e23ffffb315b3
Ahmad <ahmad.a.arrahman@gmail.com>
2015-01-20 04:13:16 +0200

    hopefully this may work

--- lms/djangoapps/manager/urls.py
+++ lms/djangoapps/manager/urls.py
@@ -12,7 +12,6 @@ urlpatterns = patterns(
     '',
     url(r'^$', sysadmin.Users.as_view(), name="sysadmin"),
     url(r'^courses/?$', sysadmin.Courses.as_view(), name="sysadmin_courses"),
-    url(r'^dump_all_users/?$', dump_all_users, name="dump_all_users"),
     url(r'^staffing/?$', sysadmin.Staffing.as_view(), name="sysadmin_staffing"),
     url(r'^gitlogs/?$', sysadmin.GitLogs.as_view(), name="gitlogs"),
     url(r'^gitlogs/(?P<course_id>.+)$', sysadmin.GitLogs.as_view(),
--- lms/djangoapps/user_uploads/views.py
+++ lms/djangoapps/user_uploads/views.py
@@ -3,34 +3,59 @@ from django.http import Http404, HttpResponseBadRequest
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST, require_GET
+from django.contrib.auth.models import User
 from django_future.csrf import ensure_csrf_cookie
 from util.json_request import JsonResponse
 from edxmako.shortcuts import render_to_response
+
+from django.http import (
+    HttpResponse, HttpResponseRedirect, HttpResponseNotFound,
+    HttpResponseBadRequest, HttpResponseForbidden, Http404
+)
 
 from shoppingcart.models import PaidCourseRegistration, Order
 from user_uploads.forms import PaymentRequestUploadForm, PaymentOrderUploadForm
 from user_uploads.models import PaymentRquestUpload, PaymentOrderUpload
 
+from manager import unicodecsv as csv
+
+# @login_required
+# @require_POST
+# def upload_payment_verification(request):
+# 	course_registration_id = request.POST.get('course_registration_id', None)
+# 	if not course_registration_id:
+# 		return HttpResponseBadRequest('No course_registration_id provided')
+
+# 	try:
+# 		course_registration = PaidCourseRegistration.objects.get(pk=course_registration_id)
+# 	except PaidCourseRegistration.DoesNotExist:
+# 		return HttpResponseBadRequest('No course registration found with this data')
+
+# 	if course_registration.user != request.user:
+# 		return HttpResponseBadRequest('You can only upload to the payment requests you own!')
+
+# 	upload = PaymentRquestUpload(user=request.user, payment_request=course_registration)
+# 	form = PaymentRequestUploadForm(request.POST, instance=upload)
+# 	form.save()
+
+# 	return redirect(reverse('dashboard'))
+
 @login_required
 @require_POST
-def upload_payment_verification(request):
-	course_registration_id = request.POST.get('course_registration_id', None)
-	if not course_registration_id:
-		return HttpResponseBadRequest('No course_registration_id provided')
+def upload_payment_verification(request):@login_required
+    """
+    download all users data needed to migrate them
+    """
+    users = User.objects.all()
+    response = HttpResponse(mimetype='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="users.csv"'
+    field_names = ['username', 'email', 'password', 'is_active', 'is_staff', 'is_superuser', 'name', 'level_of_education', 'gender', 'mailing_address', 'city', 'country', 'goals', 'year_of_birth']
+    writer = csv.DictWriter(response, fieldnames=field_names)
+    # writer.writeheader()
+    for user in users:
+        writer.writerow({'username': user.username, 'email': user.email, 'password': user.password, 'is_active': user.is_active, 'is_staff': user.is_staff, 'is_superuser': user.is_superuser, 'name': user.profile.name, 'level_of_education': user.profile.level_of_education, 'gender': user.profile.gender, 'mailing_address': user.profile.mailing_address, 'city': user.profile.city, 'country':user.profile.country, 'goals': user.profile.goals, 'year_of_birth': user.profile.year_of_birth})
 
-	try:
-		course_registration = PaidCourseRegistration.objects.get(pk=course_registration_id)
-	except PaidCourseRegistration.DoesNotExist:
-		return HttpResponseBadRequest('No course registration found with this data')
-
-	if course_registration.user != request.user:
-		return HttpResponseBadRequest('You can only upload to the payment requests you own!')
-
-	upload = PaymentRquestUpload(user=request.user, payment_request=course_registration)
-	form = PaymentRequestUploadForm(request.POST, instance=upload)
-	form.save()
-
-	return redirect(reverse('dashboard'))
+    return response
 
 @login_required
 @require_POST
