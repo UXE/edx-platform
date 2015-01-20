6b9b3bdfbbd23e737aa14a11e18139e7cfaba7c8
Ahmad <ahmad.a.arrahman@gmail.com>
2015-01-20 05:45:55 +0200

    dump_users content

--- common/djangoapps/student/management/commands/dump_users.py
+++ common/djangoapps/student/management/commands/dump_users.py
@@ -1,32 +1,18 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
-import json
+import unicodecsv as csv
 
-from student.models import UserProfile
+def dump_all_users():
+    """
+    download all users data needed to migrate them
+    """
+    users = User.objects.all()
+    with open('names.csv', 'w') as csvfile:
+      field_names = ['username', 'email', 'password', 'is_active', 'is_staff', 'is_superuser', 'name', 'level_of_education', 'gender', 'mailing_address', 'city', 'country', 'goals', 'year_of_birth']
+      writer = csv.DictWriter(csvfile, fieldnames=field_names)
+      # writer.writeheader()
+      for user in users:
+          writer.writerow({'username': user.username, 'email': user.email, 'password': user.password, 'is_active': user.is_active, 'is_staff': user.is_staff, 'is_superuser': user.is_superuser, 'name': user.profile.name, 'level_of_education': user.profile.level_of_education, 'gender': user.profile.gender, 'mailing_address': user.profile.mailing_address, 'city': user.profile.city, 'country':user.profile.country, 'goals': user.profile.goals, 'year_of_birth': user.profile.year_of_birth})
 
 
-class Command(BaseCommand):
-    help = \
-''' Extract full user information into a JSON file.
-Pass a single filename.'''
-
-    def handle(self, *args, **options):
-        f = open(args[0], 'w')
-        #text = open(args[0]).read()
-        #subject = open(args[1]).read()
-        users = User.objects.all()
-
-        l = []
-        for user in users:
-            up = UserProfile.objects.get(user=user)
-            d = {'username': user.username,
-                  'email': user.email,
-                  'is_active': user.is_active,
-                  'joined': user.date_joined.isoformat(),
-                  'name': up.name,
-                  'language': up.language,
-                  'location': up.location}
-            l.append(d)
-        json.dump(l, f)
-        f.close()
